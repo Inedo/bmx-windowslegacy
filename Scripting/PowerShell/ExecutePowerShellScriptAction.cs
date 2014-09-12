4fbc23724f55f90a309fc0c098c83b0c0a67097a
Karl Harnagy <kharnagy@inedo.com>
2014-09-12 11:37:47 -0400

    Fixed #23

--- Scripting/PowerShell/ExecutePowerShellScriptAction.cs
+++ Scripting/PowerShell/ExecutePowerShellScriptAction.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using Inedo.BuildMaster;
+using Inedo.BuildMaster.Data;
 using Inedo.BuildMaster.Extensibility;
 using Inedo.BuildMaster.Extensibility.Actions;
 using Inedo.BuildMaster.Extensibility.Actions.Scripting;
+using Inedo.BuildMaster.Extensibility.Variables;
 using Inedo.BuildMaster.Web;
-using Inedo.BuildMaster.Web.Controls.Extensions;
 using Inedo.BuildMasterExtensions.Windows.Scripting.PowerShell;
 
 namespace Inedo.BuildMasterExtensions.Windows.Shell
@@ -42,6 +43,44 @@ namespace Inedo.BuildMasterExtensions.Windows.Shell
             this.ParameterValues = BuildDictionary(parameters);
         }
 
+        protected override void Execute()
+        {
+            // Force defaults if specified in configurer
+            var configurer = (WindowsExtensionConfigurer)this.GetExtensionConfigurer();
+            if (configurer.OverridePowerShellDefaults)
+            {
+                var parameterData = StoredProcs.Scripts_GetScript(this.ScriptId)
+                    .Execute()
+                    .ScriptParameters
+                    .Where(p => !string.IsNullOrEmpty(p.DefaultValue_Text));
+
+                var application = StoredProcs.Applications_GetApplication(this.Context.ApplicationId)
+                    .Execute()
+                    .Applications_Extended
+                    .FirstOrDefault();
+
+                var evalContext = this.GetVariableEvaluationContext();
+
+                foreach (var parameter in parameterData)
+                {
+                    if (string.IsNullOrEmpty(this.ParameterValues.GetValueOrDefault(parameter.Parameter_Name)))
+                    {
+                        try
+                        {
+                            var tree = VariableExpressionTree.Parse(parameter.DefaultValue_Text, application.VariableSupport_Code);
+                            this.ParameterValues[parameter.Parameter_Name] = tree.Evaluate(evalContext);
+                        }
+                        catch
+                        {
+                            this.ParameterValues[parameter.Parameter_Name] = parameter.DefaultValue_Text;
+                        }
+                    }
+                }
+            }
+
+            base.Execute();
+        }
+
         private static IDictionary<string, string> BuildDictionary(string values)
         {
             return Util.Persistence.DeserializeToStringArray(values)
@@ -49,6 +88,18 @@ namespace Inedo.BuildMasterExtensions.Windows.Shell
                 .Where(p => p.Length == 2)
                 .Distinct(p => p[0], StringComparer.OrdinalIgnoreCase)
                 .ToDictionary(p => p[0], p => p[1], StringComparer.OrdinalIgnoreCase);
+        }
+
+        private IVariableEvaluationContext GetVariableEvaluationContext()
+        {
+            // StandardEvaluationContext should probably be moved to SDK.
+            // For now create instance using reflection.
+
+            return (IVariableEvaluationContext)Activator.CreateInstance(
+                Type.GetType("Inedo.BuildMaster.Variables.StandardVariableEvaluationContext,BuildMaster", true),
+                (IGenericBuildMasterContext)this.Context,
+                this.Context.Variables
+            );
         }
     }
 }
---
+++ WindowsExtensionConfigurer.cs
@@ -0,0 +1,20 @@
+﻿using Inedo.BuildMaster;
+using Inedo.BuildMaster.Extensibility.Configurers.Extension;
+using Inedo.BuildMaster.Web;
+
+[assembly: ExtensionConfigurer(typeof(Inedo.BuildMasterExtensions.Windows.WindowsExtensionConfigurer))]
+
+namespace Inedo.BuildMasterExtensions.Windows
+{
+    [CustomEditor(typeof(WindowsExtensionConfigurerEditor))]
+    public sealed class WindowsExtensionConfigurer : ExtensionConfigurerBase
+    {
+        [Persistent]
+        public bool OverridePowerShellDefaults { get; set; }
+
+        public override string ToString()
+        {
+            return string.Empty;
+        }
+    }
+}
